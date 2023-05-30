b471817d9ea9e3e3ad9d0443f6301b466b1fc65d
Ajarek <ajarek@poczta.onet.pl>
2023-05-31 00:29:04 +0200

    added bullets

--- src/components/Gun/Gun.jsx
+++ src/components/Gun/Gun.jsx
@@ -2,7 +2,12 @@ import React, { useRef, useEffect, useState } from 'react'
 import './Gun.css'
 const Gun = () => {
   const divRef = useRef(null)
+  const bulletsRef = useRef(null)
   const [position, setPosition] = useState(270)
+  const [range, setRange] = useState(-10)
+  const [showDiv, setShowDiv] = useState(false)
+  const [array, setArray] = useState([1,1,1,1,1])
+
 
   useEffect(() => {
     const handleKeyDown = (event) => {
@@ -15,17 +20,30 @@ const Gun = () => {
         // Sterowanie w prawo
         divRef.current.offsetLeft === 560
           ? setPosition((prevPosition) => (prevPosition = 560))
-          : setPosition((prevPosition) => prevPosition + 10)
+          : setPosition((prevPosition) => prevPosition + 10) 
+      } else if (event.key === ' ') {
+        
+        const intervalId = setInterval(() => {
+          
+         
+            setRange((prevPosition) => prevPosition - 10)
+        }, 200)
+
+        setShowDiv(true)
+        return () => {
+          clearInterval(intervalId)
+        }
       }
     }
 
     window.addEventListener('keydown', handleKeyDown)
-
+   
     return () => {
       window.removeEventListener('keydown', handleKeyDown)
     }
   }, [])
-
+  range === -320
+  ? setRange((prevPosition) => (prevPosition = -10)):null
   return (
     <div
       className='gun'
@@ -35,7 +53,23 @@ const Gun = () => {
         left: `${position}px`,
         bottom: '0px',
       }}
-    ></div>
+    >
+      {showDiv && (
+        array.map((item,index) =>
+        <div
+          key={index}
+          className='bullets'
+          ref={bulletsRef}
+          style={{
+            position: 'absolute',
+            top: `${range*index}px`,
+            left: '50%',
+          }}
+        >
+         <img src="/rocket.png" alt="" />
+        </div>)
+      )}
+    </div>
   )
 }
 
